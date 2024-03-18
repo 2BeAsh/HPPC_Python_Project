6677bc36bb4b2528deb726ba64d861de8dcf5fcd
idarye <123239382+idarye@users.noreply.github.com>
2024-03-18 14:56:15 +0100

    adding job.sh

---
+++ Code/job.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=mpi4py
+#SBATCH --partition=modi_HPPC
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+
+mpirun apptainer exec \
+ ~/modi_images/ucphhpc/hpc-notebook:latest \
+ ~/modi_mount/new_env/bin/python dask_test.py
